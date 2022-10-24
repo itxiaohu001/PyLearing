775fb9508fc766b2c91c7c22e6ab38d1d9f0dc99
huyongfeng <huyf@anpro-tech.com>
2022-10-24 19:45:18 +0800

    alien_invasion start preliminarily

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,5 +1,9 @@
 class Settings:
-    def _init_(self):
-        self.screen_wigth = 1200
-        self.screen_heigth = 800
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
